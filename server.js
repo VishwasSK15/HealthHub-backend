9ff14597467f42f1c81ba2b051289d971772dcbf
VishwasSK15 <vishwasskshikaripura@gmail.com>
2025-11-13 19:35:31 +0530

    Fix cors

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ["https://health-hub-frontend-five.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 // api endpoints
 app.use("/api/user", userRouter)
